efe73a7294c310234b58e394d6d874a263f50848
Nicolas Viveros <nviverosb@unal.edu.co>
2018-05-12 14:09:24 -0500

    Updates

--- Tests/Assets/Scripts/IMU.cs
+++ Tests/Assets/Scripts/IMU.cs
@@ -12,56 +12,34 @@ public class IMU : MonoBehaviour {
 	public static Vector3 angularVelocity,accel;
 	public static double Yaw, Roll, Pitch;
 
-	private static float[] quaternion;
+	private float[] quaternion;
+	private float[] eInt;
 //	public double aRoll, aPitch, magX, magY, time;
 	private double time;
 	private float Beta;
+	private float Kp = 1, Ki = 0;
 	private UDPClient server;
 	private Vector3 eA;//SHORT FOR EULERANGLES, UT SINCE IT'S CALLED A LOT, MIGHT AS WELL ABREVIATE THE NAME
 	private AHRS ahrs;
+	private Vector3 actualPosition;
 
 	// Use this for initialization
 	void Start () {
 
 		quaternion = new float[] { 1f, 0f, 0f, 0f };
+		eInt = new float[] { 0f, 0f, 0f };
+		actualPosition = transform.position;
 //		SamplePeriod = 1f/131f;
-		server = FindObjectOfType<UDPClient> ();
-		angularVelocity = accel = Vector3.zero;
-		Yaw = Roll = Pitch = 1;
-		time = 0;
 		Beta = 0.1f;
-	}
-
-	//Equations taken from: https://github.com/xioTechnologies/Open-Source-AHRS-With-x-IMU
-	Vector3 ComputeRotation(){
-
-		double t = Time.time;
-		if(server.IsActive()){
-			
-			if (double.IsNaN(Yaw) || double.IsNaN(Roll)) {
-				Yaw = 1;
-				Roll = 1;
-			} 
-			else {
-				Roll = Math.Atan (-Transmitter.Gaccel.x/Transmitter.Gaccel.z) * Mathf.Rad2Deg;
-				Yaw = Math.Atan (Transmitter.Gaccel.y/Math.Sqrt(Math.Pow(Transmitter.Gaccel.x,2) + Math.Pow(Transmitter.Gaccel.z,2))) * Mathf.Rad2Deg;
-			}
-
-			Pitch = Math.Atan(-Transmitter.magnetometer.y/Transmitter.magnetometer.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.Euler (new Vector3((float) Roll,(float)Pitch, (float) Yaw));
-		}
-		time = Time.time - t;
-
-		//DONT KNOW WHY, BUT  PITCH->X, YAW
-		return new Vector3((float) Roll,(float)Pitch, (float) Yaw);
 	}
 
 	void Update () {
 
-		CalculateAngles ();
+//		UpdateRotation();
+		UpdatePositionAndRotation();
 	}
 
-	void CalculateAngles(){
+	void UpdatePositionAndRotation(){
 
 		float gx, gy, gz, ax, ay, az, mx, my, mz;
 		gx = Transmitter.gyroscope.x;	
@@ -76,23 +54,12 @@ public class IMU : MonoBehaviour {
 
 		float q1 = quaternion[0], q2 = quaternion[1], q3 = quaternion[2], q4 = quaternion[3];   // short name local variable for readability
 		float norm;
-		float hx, hy, _2bx, _2bz;
-		float s1, s2, s3, s4;
-		float qDot1, qDot2, qDot3, qDot4;
+		float hx, hy, bx, bz;
+		float vx, vy, vz, wx, wy, wz;
+		float ex, ey, ez;
+		float pa, pb, pc;
 
 		// Auxiliary variables to avoid repeated arithmetic
-		float _2q1mx;
-		float _2q1my;
-		float _2q1mz;
-		float _2q2mx;
-		float _4bx;
-		float _4bz;
-		float _2q1 = 2f * q1;
-		float _2q2 = 2f * q2;
-		float _2q3 = 2f * q3;
-		float _2q4 = 2f * q4;
-		float _2q1q3 = 2f * q1 * q3;
-		float _2q3q4 = 2f * q3 * q4;
 		float q1q1 = q1 * q1;
 		float q1q2 = q1 * q2;
 		float q1q3 = q1 * q3;
@@ -102,7 +69,7 @@ public class IMU : MonoBehaviour {
 		float q2q4 = q2 * q4;
 		float q3q3 = q3 * q3;
 		float q3q4 = q3 * q4;
-		float q4q4 = q4 * q4;
+		float q4q4 = q4 * q4;   
 
 		// Normalise accelerometer measurement
 		norm = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
@@ -121,46 +88,136 @@ public class IMU : MonoBehaviour {
 		mz *= norm;
 
 		// Reference direction of Earth's magnetic field
-		_2q1mx = 2f * q1 * mx;
-		_2q1my = 2f * q1 * my;
-		_2q1mz = 2f * q1 * mz;
-		_2q2mx = 2f * q2 * mx;
-		hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4;
-		hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4;
-		_2bx = (float)Math.Sqrt(hx * hx + hy * hy);
-		_2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4;
-		_4bx = 2f * _2bx;
-		_4bz = 2f * _2bz;
+		hx = 2f * mx * (0.5f - q3q3 - q4q4) + 2f * my * (q2q3 - q1q4) + 2f * mz * (q2q4 + q1q3);
+		hy = 2f * mx * (q2q3 + q1q4) + 2f * my * (0.5f - q2q2 - q4q4) + 2f * mz * (q3q4 - q1q2);
+		bx = (float)Math.Sqrt((hx * hx) + (hy * hy));
+		bz = 2f * mx * (q2q4 - q1q3) + 2f * my * (q3q4 + q1q2) + 2f * mz * (0.5f - q2q2 - q3q3);
 
-		// Gradient decent algorithm corrective step
-		s1 = -_2q3 * (2f * q2q4 - _2q1q3 - ax) + _2q2 * (2f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-		s2 = _2q4 * (2f * q2q4 - _2q1q3 - ax) + _2q1 * (2f * q1q2 + _2q3q4 - ay) - 4f * q2 * (1 - 2f * q2q2 - 2f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-		s3 = -_2q1 * (2f * q2q4 - _2q1q3 - ax) + _2q4 * (2f * q1q2 + _2q3q4 - ay) - 4f * q3 * (1 - 2f * q2q2 - 2f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-		s4 = _2q2 * (2f * q2q4 - _2q1q3 - ax) + _2q3 * (2f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-		norm = 1f / (float)Math.Sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
-		s1 *= norm;
-		s2 *= norm;
-		s3 *= norm;
-		s4 *= norm;
+		// Estimated direction of gravity and magnetic field
+		vx = 2f * (q2q4 - q1q3);
+		vy = 2f * (q1q2 + q3q4);
+		vz = q1q1 - q2q2 - q3q3 + q4q4;
+		wx = 2f * bx * (0.5f - q3q3 - q4q4) + 2f * bz * (q2q4 - q1q3);
+		wy = 2f * bx * (q2q3 - q1q4) + 2f * bz * (q1q2 + q3q4);
+		wz = 2f * bx * (q1q3 + q2q4) + 2f * bz * (0.5f - q2q2 - q3q3);  
 
-		// Compute rate of change of quaternion
-		qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - Beta * s1;
-		qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - Beta * s2;
-		qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - Beta * s3;
-		qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - Beta * s4;
+		// Error is cross product between estimated direction and measured direction of gravity
+		ex = (ay * vz - az * vy) + (my * wz - mz * wy);
+		ey = (az * vx - ax * vz) + (mz * wx - mx * wz);
+		ez = (ax * vy - ay * vx) + (mx * wy - my * wx);
+		if (Ki > 0f)
+		{
+			eInt[0] += ex;      // accumulate integral error
+			eInt[1] += ey;
+			eInt[2] += ez;
+		}
+		else
+		{
+			eInt[0] = 0.0f;     // prevent integral wind up
+			eInt[1] = 0.0f;
+			eInt[2] = 0.0f;
+		}
 
-		// Integrate to yield quaternion
-		q1 += qDot1 * SamplePeriod;
-		q2 += qDot2 * SamplePeriod;
-		q3 += qDot3 * SamplePeriod;
-		q4 += qDot4 * SamplePeriod;
-		norm = 1f / (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
+		// Apply feedback terms
+		gx = gx + Kp * ex + Ki * eInt[0];
+		gy = gy + Kp * ey + Ki * eInt[1];
+		gz = gz + Kp * ez + Ki * eInt[2];
+
+		// Integrate rate of change of quaternion
+		pa = q2;
+		pb = q3;
+		pc = q4;
+		q1 = q1 + (-q2 * gx - q3 * gy - q4 * gz) * (0.5f * SamplePeriod);
+		q2 = pa + (q1 * gx + pb * gz - pc * gy) * (0.5f * SamplePeriod);
+		q3 = pb + (q1 * gy - pa * gz + pc * gx) * (0.5f * SamplePeriod);
+		q4 = pc + (q1 * gz + pa * gy - pb * gx) * (0.5f * SamplePeriod);
+
+		// Normalise quaternion
+		norm = (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);
+		norm = 1.0f / norm;
 		quaternion[0] = q1 * norm;
 		quaternion[1] = q2 * norm;
 		quaternion[2] = q3 * norm;
 		quaternion[3] = q4 * norm;
 
+//		print (pa*2 + " " + pb*100 + " " + pc*100);
 		Quaternion rotation = new Quaternion (quaternion[2],quaternion[0],quaternion[1],quaternion[3]);
-		transform.rotation = rotation;
+		transform.localRotation = rotation;
+//		transform.localPosition = new Vector3(pa*10,pb*10,pc*10) + actualPosition;
+	}
+
+	//Code taken from: https://github.com/xioTechnologies/Open-Source-AHRS-With-x-IMU/blob/master/x-IMU%20IMU%20and%20AHRS%20Algorithms/x-IMU%20IMU%20and%20AHRS%20Algorithms/AHRS/MahonyAHRS.cs
+	public void UpdateRotation()
+	{
+		float gx, gy, gz, ax, ay, az;
+		gx = Transmitter.gyroscope.x;	
+		gy = Transmitter.gyroscope.y;
+		gz = Transmitter.gyroscope.z;
+		ax = Transmitter.accelerometer.x;	
+		ay = Transmitter.accelerometer.y;
+		az = Transmitter.accelerometer.z;
+
+		float q1 = quaternion[0], q2 = quaternion[1], q3 = quaternion[2], q4 = quaternion[3];   // short name local variable for readability
+		float norm;
+		float vx, vy, vz;
+		float ex, ey, ez;
+		float pa, pb, pc;
+
+		// Normalise accelerometer measurement
+		norm = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
+		if (norm == 0f) return; // handle NaN
+		norm = 1 / norm;        // use reciprocal for division
+		ax *= norm;
+		ay *= norm;
+		az *= norm;
+
+		// Estimated direction of gravity
+		vx = 2.0f * (q2 * q4 - q1 * q3);
+		vy = 2.0f * (q1 * q2 + q3 * q4);
+		vz = q1 * q1 - q2 * q2 - q3 * q3 + q4 * q4;
+
+		// Error is cross product between estimated direction and measured direction of gravity
+		ex = (ay * vz - az * vy);
+		ey = (az * vx - ax * vz);
+		ez = (ax * vy - ay * vx);
+		if (Ki > 0f)
+		{
+			eInt[0] += ex;      // accumulate integral error
+			eInt[1] += ey;
+			eInt[2] += ez;
+		}
+		else
+		{
+			eInt[0] = 0.0f;     // prevent integral wind up
+			eInt[1] = 0.0f;
+			eInt[2] = 0.0f;
+		}
+
+		// Apply feedback terms
+		gx = gx + Kp * ex + Ki * eInt[0];
+		gy = gy + Kp * ey + Ki * eInt[1];
+		gz = gz + Kp * ez + Ki * eInt[2];
+
+		// Integrate rate of change of quaternion
+		pa = q2;
+		pb = q3;
+		pc = q4;
+		q1 = q1 + (-q2 * gx - q3 * gy - q4 * gz) * (0.5f * SamplePeriod);
+		q2 = pa + (q1 * gx + pb * gz - pc * gy) * (0.5f * SamplePeriod);
+		q3 = pb + (q1 * gy - pa * gz + pc * gx) * (0.5f * SamplePeriod);
+		q4 = pc + (q1 * gz + pa * gy - pb * gx) * (0.5f * SamplePeriod);
+
+		// Normalise quaternion
+		norm = (float)Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);
+		norm = 1.0f / norm;
+		quaternion[0] = q1 * norm;
+		quaternion[1] = q2 * norm;
+		quaternion[2] = q3 * norm;
+		quaternion[3] = q4 * norm;
+
+		print (q2*100 + " " + q3*100 + " " + q4*100);
+		Quaternion rotation = new Quaternion (quaternion[2],quaternion[0],quaternion[1],quaternion[3]);
+		transform.localRotation = rotation;
+//		transform.localPosition = new Vector3(gx*5,gy*5,gz*5) + actualPosition;
 	}
 }
